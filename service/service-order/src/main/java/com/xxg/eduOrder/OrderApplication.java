71c9bee9728876af8e93c8a4b0659baa29f8a635
xuxinggang <2216557012@qq.com>
2021-01-23 14:41:05 +0800

    微信支付V-1.0

---
+++ service/service-order/src/main/java/com/xxg/eduOrder/OrderApplication.java
@@ -0,0 +1,18 @@
+package com.xxg.eduOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xxg
+ * @version 1.0
+ * @date 2021/1/23 10:30
+ * @Description:
+ * @Params:
+ */
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class,args);
+    }
+}
